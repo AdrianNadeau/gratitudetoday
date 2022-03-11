e605b15294ec68a1d688e0b11071323dd5b9ae2e
DESKTOP-KUL07K0\adria <adrian@adriannadeau.com>
2022-03-10 21:17:02 -0400

    update code

--- routes/route-upload.js
+++ routes/route-upload.js
@@ -13,14 +13,9 @@
  (Post = require("../models/PostModel")),
  (mongoose = require("mongoose")),
  (bcrypt = require("bcryptjs"));
-
- 
-logger.debug("set creds");
  const AWS = require('aws-sdk');
  const UUID = require('uuid/v4');
- const Busboy = require('busboy');
- AWS.config.update({ accessKeyId: "AKIA37SVVXBHZG7RHHXQ", secretAccessKey: "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", region:"us-east-1" });
- const S3 = new AWS.S3();
+const S3 = new AWS.S3();
  
  /**
   * route where we get multipart form data
@@ -35,13 +30,30 @@ logger.debug("set creds");
     res.render("avatarError", { url: "accounts" });
   });
  router.post("/upload", (req, res) => {
-     // Load the SDK for JavaScript
-    var AWS = require('aws-sdk');
-    // Set the Region 
-    AWS.config.update({region: 'us-east-1'});
-
-    // Create S3 service object
-    s3 = new AWS.S3({apiVersion: '2006-03-01'});
+   logger.debug("UPLOAD");
+  var params = {Bucket: 'gratitudetoday', Key: 'key', Body: stream};
+  s3.upload(params, function(err, data) {
+    console.log(err, data);
+  });
+  console.log("data:"+data)
+  //  logger.debug("upload image:"+req.params.userId);
+  // var buf = Buffer.from(req.body.imageBinary.replace(/^data:image\/\w+;base64,/, ""),'base64')
+  // logger.debug('buf: '+buf);
+  // var data = {
+  //   Key: req.body.userId, 
+  //   Body: buf,
+  //   ContentEncoding: 'base64',
+  //   ContentType: 'image/jpeg'
+  // };
+  // logger.debug('data: '+data);
+  // s3Bucket.putObject(data, function(err, data){
+  //     if (err) { 
+  //       console.log(err);
+  //       console.log('Error uploading data: ', data); 
+  //     } else {
+  //       console.log('successfully uploaded the image!');
+  //     }
+  // });
     // AWS.config.getCredentials(function(err) {
     //     if (err) console.log(err.stack);
     //     // credentials not loaded
@@ -96,58 +108,6 @@ logger.debug("set creds");
     //  req.pipe(busboy);
  });
 
+
+
 module.exports = router;
-
-// const express = require("express");
-// const router = express.Router();
-// var fs = require('fs');
-// logger = require("../logger/logger"),
-// router.use(express.json());
-// var AWS = require('aws-sdk');
-
-// router.post("/updateAvatar", function (req, res) {
-//     // Load the AWS SDK for Node.js
-//     logger.debug("upload avatar")
-//     // Set the region 
-//     AWS.config.update({region: process.env.REGION});
-//     logger.debug("region")
-//     // Create S3 service object
-//     var s3 = new AWS.S3({apiVersion: '2006-03-01'});
-//     logger.debug("api")
-//     // call S3 to retrieve upload file to specified bucket
-//     var uploadParams = {Bucket: process.env.BUCKET_NAME, Key: '', Body: ''};
-//     var file = '/home/adrian/repos/gratitudetoday/README.md';
-//     logger.debug("file"+file)
-
-// // Configure the file stream and obtain the upload parameters
-    
-//     var fileStream = fs.createReadStream(file);
-//     fileStream.on('error', function(err) {
-//     console.log('File Error', err);
-// }); 
-//     uploadParams.Body = fileStream;
-//     uploadParams.Key = path.basename(file);
-
-//     // call S3 to retrieve upload file to specified bucket
-//     s3.upload (uploadParams, function (err, data) {
-//     if (err) {
-//         console.log("Error", err);
-//     } if (data) {
-//         console.log("Upload Success", data.Location);
-//     }
-//     });
-// //     AWS.config.update({region: process.env.REGION});
-// //     // Create S3 service object
-// //     s3 = new AWS.S3({apiVersion: '2006-03-01'});
-
-// //     // Call S3 to list the buckets
-// //     s3.listBuckets(function(err, data) {
-// //     if (err) {
-// //         console.log("Error", err);
-// //     } else {
-// //         console.log("Success", data.Buckets);
-// //     }
-// // });
-
-// });
-// module.exports = router;
